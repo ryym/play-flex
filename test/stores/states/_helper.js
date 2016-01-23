63f3447ec5a49eff659caad37360c52396702d42
ryym <ryym.64@gmail.com>
2016-01-23 16:29:47 +0900

    test(states): Add helper of state manager tests

---
+++ test/stores/states/_helper.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview Provide helper functions used to test state manager classes.
+ */
+
+/**
+ * Extend a given state class for testing and return its subclass.
+ * @param {StateClass} StateClass - The state class to be extended.
+ * @param {Object} initialState - The initial state of the instance.
+ * @return {StateClass} The subclass of the given class.
+ */
+export function createTestState(StateClass, initialState) {
+  class TestState extends StateClass {
+    constructor() {
+      super();
+      this.state = initialState;
+    }
+
+    getState() {
+      return this.state;
+    }
+
+    setState(nextState) {
+      this.state = nextState;
+    }
+  }
+
+  return TestState;
+}
