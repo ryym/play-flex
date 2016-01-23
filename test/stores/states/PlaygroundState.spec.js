35efbbfcdde562aa8e3f08e44b1b13d2cee509ba
ryym <ryym.64@gmail.com>
2016-01-23 23:52:49 +0900

    test(Playground): Change initial state

--- test/stores/states/PlaygroundState.spec.js
+++ test/stores/states/PlaygroundState.spec.js
@@ -1,31 +1,33 @@
 import assert from 'power-assert';
-import { createTestState } from './_helper';
+import FlexContainer from '$app/models/FlexContainer';
 import PlaygroundState from '$app/stores/states/PlaygroundState';
+import { createTestState } from './_helper';
 
-const rootContainer = 'root-container';
-
-const TestPlaygroundState = createTestState(PlaygroundState, {
-  rootContainer,
-  containers: {},
-  items: {}
-});
+const TestPlaygroundState = createTestState(PlaygroundState);
 
 /**
  * @test {PlaygroundState}
  */
 describe('PlaygroundState', function() {
+  const rootId = 'root-container';
+  let rootContainer;
   let playground;
 
   beforeEach(() => {
-    playground = new TestPlaygroundState();
+    rootContainer = new FlexContainer(rootId);
+    playground = new TestPlaygroundState({
+      rootContainerId: rootId,
+      containers: { [rootId]: rootContainer },
+      items: {}
+    });
   });
 
   /** @test {PlaygroundState#constructor} */
   it('has an initial state', () => {
     const state = playground.getState();
     assert.deepEqual(state, {
-      rootContainer,
-      containers: {},
+      rootContainerId: rootId,
+      containers: { [rootId]: rootContainer },
       items: {}
     });
   });
