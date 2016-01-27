a7f9f6f37ef68adb1789dd91ef0707ee38eec953
ryym <ryym.64@gmail.com>
2016-01-27 23:10:06 +0900

    feat(BoxTree): Add map func which traverse from parent

--- src/app/shared/states/BoxTreeStateMapper.js
+++ src/app/shared/states/BoxTreeStateMapper.js
@@ -3,6 +3,7 @@ export default class BoxTreeStateMapper {
   constructor(state) {
     this.state = state;
     this.mapRecursively = this.mapRecursively.bind(this);
+    this.mapFromParent = this.mapFromParent.bind(this);
   }
 
   mapRecursively(mapper, childReducer) {
@@ -21,4 +22,32 @@ export default class BoxTreeStateMapper {
     return mapper(box, childs);
   }
 
+  /**
+   * Traverse box tree from top to down. Using this method,
+   * you can pass some arguments to children from its parent.
+   * @param {function} mapBox - The mapper of each box.
+   * @param {*} props - The prop passed to the root box.
+   * @return {*} A tree consists of each mapped value.
+   * @example
+   * function printEachBox(box, parent, printEachBox, getChildsOf) {
+   *   const boxId = box.getId();
+   *   const mappedChilds = getChildsOf(box).map(child => {
+   *     return printEachBox(child, boxId, printEachBox, getChildsOf);
+   *   });
+   *   console.log(`The parent of ${boxId} is ${parent}!`);
+   * }
+   * // Print each box id with its parent id.
+   * const mapper = new BoxTreeStateMapper(state);
+   * mapper.mapFromParent(printEachBox, 'not present');
+   */
+  mapFromParent(mapBox, props) {
+    const root = this.state.boxes[this.state.rootId];
+    return mapBox(root, props, mapBox, this.getChildsOf.bind(this));
+  }
+
+  getChildsOf(box) {
+    return box.getChildIds().map(id => {
+      return this.state.boxes[id];
+    });
+  }
 }
--- test/app/shared/states/BoxTreeStateMapper.spec.js
+++ test/app/shared/states/BoxTreeStateMapper.spec.js
@@ -85,4 +85,98 @@ describe('BoxTreeStateMapper', function() {
       }
     );
   });
+
+  /** @test {BoxTreeStateMapper#mapFromParent} */
+  describe('#mapFromParent', () => {
+    function mapBox(box, parent, mapBox, getChildsOf) {
+      const data = getChildsOf(box).reduce((data, child) => {
+        return Object.assign(
+          data, mapBox(child, box.getId(), mapBox, getChildsOf)
+        );
+      }, { p: parent });
+      return {
+        [box.getId()]: data
+      };
+    }
+
+    function testMapFromParent(...expectations) {
+      expectations.forEach(({ skip, state, result }) => {
+        if (skip) {
+          return;
+        }
+        it(`${JSON.stringify(result)}`, () => {
+          const mapper = new BoxTreeStateMapper(state);
+          const actualResult = mapper.mapFromParent(mapBox);
+
+          assert.deepEqual(actualResult, result);
+        });
+      });
+    }
+
+    testMapFromParent(
+      {
+        state: h.makeBoxTree(
+          ['1', ['2']],
+          ['2']
+        ),
+        result: {
+          '1': {
+            '2': { p: '1' },
+            p: undefined
+          }
+        }
+      },
+      {
+        state: h.makeBoxTree(
+          ['1', ['2', '3']],
+          ['2', ['4']],
+          ['3'],
+          ['4']
+        ),
+        result: {
+          '1': {
+            '2': {
+              '4': { p: '2' },
+              p: '1'
+            },
+            '3': { p: '1' },
+            p: undefined
+          }
+        }
+      },
+      {
+        state: h.makeBoxTree(
+          ['1', ['2']],
+          ['2', ['4', '5']],
+          ['3', ['8']],
+          ['4'],
+          ['5', ['6', '7']],
+          ['6', ['3']],
+          ['7'],
+          ['8']
+        ),
+        result: {
+          '1': {
+            '2': {
+              '4': { p: '2' },
+              '5': {
+                '6': {
+                  '3': {
+                    '8': { p: '3' },
+                    p: '6'
+                  },
+                  p: '5'
+                },
+                '7': { p: '5' },
+                p: '2'
+              },
+              p: '1'
+            },
+            p: undefined
+          }
+        }
+      }
+    );
+  });
+
 });
